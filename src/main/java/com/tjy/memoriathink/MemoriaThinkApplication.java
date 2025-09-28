29b1ca7f299dad07a1e2a0fa8f3f6e707af679fa
TJY <2186987211@qq.com>
2025-09-28 18:09:55 +0800

    初次提交代码

---
+++ src/main/java/com/tjy/memoriathink/MemoriaThinkApplication.java
@@ -0,0 +1,13 @@
+package com.tjy.memoriathink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriaThinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoriaThinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjy/memoriathink/MemoriaThinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjy.memoriathink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoriaThinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
